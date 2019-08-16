af95235acb15774f396cdf4b1df621cb7c61f60c
Wuu <coolwuu0207@gmail.com>
2019-08-16 16:13:08 +0800

    Add skipwhile test case

--- CSharpAdvanceDesignTests/JoeySkipWhileTests.cs
+++ CSharpAdvanceDesignTests/JoeySkipWhileTests.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using ExpectedObjects;
+﻿using ExpectedObjects;
+using Lab;
 using Lab.Entities;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
 using System.Collections.Generic;
 using System.Linq;
-using Lab;
 
 namespace CSharpAdvanceDesignTests
 {
@@ -37,6 +36,32 @@ namespace CSharpAdvanceDesignTests
             };
 
             expected.ToExpectedObject().ShouldMatch(actual.ToList());
+        }
+
+        [Test]
+        public void skip_cards_when_point_less_than_5()
+        {
+            var cards = new List<Card>
+            {
+                new Card {Kind = CardKind.Normal, Point = 2},
+                new Card {Kind = CardKind.Normal, Point = 3},
+                new Card {Kind = CardKind.Normal, Point = 4},
+                new Card {Kind = CardKind.Separate},
+                new Card {Kind = CardKind.Normal, Point = 5},
+                new Card {Kind = CardKind.Normal, Point = 6},
+                new Card {Kind = CardKind.Separate},
+            };
+            var actual = cards.JoeySkipWhile(card => card.Point < 5);
+
+
+            var expected = new List<Card>
+            {
+                new Card {Kind = CardKind.Normal, Point = 5},
+                new Card {Kind = CardKind.Normal, Point = 6},
+                new Card {Kind = CardKind.Separate},
+            };
+
+            expected.ToExpectedObject().ShouldMatch(actual);
         }
     }
 }
