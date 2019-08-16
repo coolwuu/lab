2cdfc166435f60decffe70676d31bb89613fc5ca
Wuu <coolwuu0207@gmail.com>
2019-08-16 09:42:46 +0800

    move product to field

--- CSharpAdvanceDesignTests/JoeyWhereTests.cs
+++ CSharpAdvanceDesignTests/JoeyWhereTests.cs
@@ -10,22 +10,22 @@ namespace CSharpAdvanceDesignTests
     [TestFixture()]
     public class JoeyWhereTests
     {
+        private readonly List<Product> _products = new List<Product>
+        {
+            new Product {Id = 1, Cost = 11, Price = 110, Supplier = "Odd-e"},
+            new Product {Id = 2, Cost = 21, Price = 210, Supplier = "Yahoo"},
+            new Product {Id = 3, Cost = 31, Price = 310, Supplier = "Odd-e"},
+            new Product {Id = 4, Cost = 41, Price = 410, Supplier = "Odd-e"},
+            new Product {Id = 5, Cost = 51, Price = 510, Supplier = "Momo"},
+            new Product {Id = 6, Cost = 61, Price = 610, Supplier = "Momo"},
+            new Product {Id = 7, Cost = 71, Price = 710, Supplier = "Yahoo"},
+            new Product {Id = 8, Cost = 18, Price = 780, Supplier = "Yahoo"}
+        };
+
         [Test]
         public void find_products_that_price_between_200_and_500()
         {
-            var products = new List<Product>
-            {
-                new Product {Id = 1, Cost = 11, Price = 110, Supplier = "Odd-e"},
-                new Product {Id = 2, Cost = 21, Price = 210, Supplier = "Yahoo"},
-                new Product {Id = 3, Cost = 31, Price = 310, Supplier = "Odd-e"},
-                new Product {Id = 4, Cost = 41, Price = 410, Supplier = "Odd-e"},
-                new Product {Id = 5, Cost = 51, Price = 510, Supplier = "Momo"},
-                new Product {Id = 6, Cost = 61, Price = 610, Supplier = "Momo"},
-                new Product {Id = 7, Cost = 71, Price = 710, Supplier = "Yahoo"},
-                new Product {Id = 8, Cost = 18, Price = 780, Supplier = "Yahoo"}
-            };
-
-            var actual = JoeyWhere(products, product => product.Price > 200 && product.Price < 500);
+            var actual = JoeyWhere(_products, product => product.Price > 200 && product.Price < 500);
 
             var expected = new List<Product>
             {
@@ -40,19 +40,7 @@ namespace CSharpAdvanceDesignTests
         [Test]
         public void find_products_that_price_between_200_and_500_and_cost_less_than_30()
         {
-            var products = new List<Product>
-            {
-                new Product {Id = 1, Cost = 11, Price = 110, Supplier = "Odd-e"},
-                new Product {Id = 2, Cost = 21, Price = 210, Supplier = "Yahoo"},
-                new Product {Id = 3, Cost = 31, Price = 310, Supplier = "Odd-e"},
-                new Product {Id = 4, Cost = 41, Price = 410, Supplier = "Odd-e"},
-                new Product {Id = 5, Cost = 51, Price = 510, Supplier = "Momo"},
-                new Product {Id = 6, Cost = 61, Price = 610, Supplier = "Momo"},
-                new Product {Id = 7, Cost = 71, Price = 710, Supplier = "Yahoo"},
-                new Product {Id = 8, Cost = 18, Price = 780, Supplier = "Yahoo"}
-            };
-
-            var actual = JoeyWhere(products,
+            var actual = JoeyWhere(_products,
                 product => product.Price > 200 && product.Price < 500 && product.Cost < 30);
 
             var expected = new List<Product>
