86f8da7d1b0f6c719ecab8d2266b72a5eb0466be
Wuu <coolwuu0207@gmail.com>
2019-08-17 13:45:18 +0800

    Average

--- CSharpAdvanceDesignTests/JoeyAverageTests.cs
+++ CSharpAdvanceDesignTests/JoeyAverageTests.cs
@@ -5,22 +5,57 @@ using System.Collections.Generic;
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture()]
-    [Ignore("not yet")]
     public class JoeyAverageTests
     {
         [Test]
-        public void average_with_null_value()
+        public void average_with_value()
         {
-            var numbers = new int?[] { 2, 4, null, 6 };
+            var numbers = new int?[] { 2, 4, 6 };
 
             var actual = JoeyAverage(numbers);
 
-            //var expected = ?
+            var expected = 4;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void average_with_value1()
+        {
+            var numbers = new int?[] { 2, 4, 6, 6 };
+
+            var actual = JoeyAverage(numbers);
+
+            var expected = 4.5;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void average_with_null_value()
+        {
+            var numbers = new int?[] { 2, 4, 6, null };
+
+            var actual = JoeyAverage(numbers);
+
+            var expected = 4;
+            Assert.AreEqual(expected, actual);
         }
 
         private double? JoeyAverage(IEnumerable<int?> numbers)
         {
-            throw new System.NotImplementedException();
+            var enumerator = numbers.GetEnumerator();
+            double? sum = 0;
+            int count = 0;
+            while (enumerator.MoveNext())
+            {
+                var number = enumerator.Current;
+                if (number.HasValue)
+                {
+                    sum += number;
+                    count++;
+                }
+            }
+
+            return sum / count;
         }
     }
 }
