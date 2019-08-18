17c07e820c3b87f2cc8fa42d7df6fb884770d7a5
Wuu <coolwuu0207@gmail.com>
2019-08-18 17:38:58 +0800

    SkipLast

--- CSharpAdvanceDesignTests/JoeySkipTests.cs
+++ CSharpAdvanceDesignTests/JoeySkipTests.cs
@@ -33,5 +33,45 @@ namespace CSharpAdvanceDesignTests
 
             expected.ToExpectedObject().ShouldMatch(actual.ToList());
         }
+
+        [Test]
+        public void skip_last_2()
+        {
+            var numbers = new[] { 10, 20, 30, 40, 50 };
+            var actual = JoeySkipLast(numbers, 2);
+
+            var expected = new[] { 10, 20, 30 };
+
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        private IEnumerable<int> JoeySkipLast(IEnumerable<int> numbers, int count)
+        {
+            //var enumerator = numbers.GetEnumerator();
+            //var totalCount = numbers.Count();
+            //var counter = 1;
+            //while (enumerator.MoveNext())
+            //{
+            //    if (counter <= totalCount - count)
+            //    {
+            //        var current = enumerator.Current;
+            //        yield return current;
+            //    }
+
+            //    counter++;
+            //}
+            var queue = new Queue<int>();
+            var enumerator = numbers.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                var current = enumerator.Current;
+                if (queue.Count == count)
+                {
+                    yield return queue.Dequeue();
+                }
+
+                queue.Enqueue(current);
+            }
+        }
     }
 }
