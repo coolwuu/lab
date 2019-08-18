4eb2c4b0977c56e0ee2147b69dc47e438ef35466
Wuu <coolwuu0207@gmail.com>
2019-08-18 15:28:23 +0800

    except and intersect

--- CSharpAdvanceDesignTests/JoeyIntersectTests.cs
+++ CSharpAdvanceDesignTests/JoeyIntersectTests.cs
@@ -5,14 +5,13 @@ using System.Collections.Generic;
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture]
-    [Ignore("not yet")]
     public class JoeyIntersectTests
     {
         [Test]
         public void intersect_numbers()
         {
-            var first = new[] { 1, 3, 5 };
-            var second = new[] { 5, 7, 3 };
+            var first = new[] { 1, 3, 5, 1 };
+            var second = new[] { 5, 7, 3, 3 };
 
             var actual = JoeyIntersect(first, second);
 
@@ -21,9 +20,21 @@ namespace CSharpAdvanceDesignTests
             expected.ToExpectedObject().ShouldMatch(actual);
         }
 
-        private IEnumerable<int> JoeyIntersect(IEnumerable<int> first, IEnumerable<int> second)
+        private static IEnumerable<int> JoeyIntersect(IEnumerable<int> first, IEnumerable<int> second)
         {
-            throw new System.NotImplementedException();
+            //add IEnumerable<T> into hashSet can get a distinct-ed hashSet
+            var secondHashSet = new HashSet<int>(second);
+            var firstHashSet = new HashSet<int>(first);
+
+            var firstEnumerator = firstHashSet.GetEnumerator();
+            while (firstEnumerator.MoveNext())
+            {
+                var current = firstEnumerator.Current;
+                if (!secondHashSet.Add(current))
+                {
+                    yield return current;
+                }
+            }
         }
     }
 }
