d9a847f89dfedb11bee2b7c4327c9fc7c76af16d
Wuu <coolwuu0207@gmail.com>
2019-08-22 00:12:22 +0800

    [Orderby] Extract secondKeySelector and secondKeyComparer

--- CSharpAdvanceDesignTests/JoeyOrderByTests.cs
+++ CSharpAdvanceDesignTests/JoeyOrderByTests.cs
@@ -44,7 +44,7 @@ namespace CSharpAdvanceDesignTests
                 new Employee {FirstName = "Joey", LastName = "Chen"},
             };
 
-            var actual = JoeyOrderByLastNameAndFirstName(employees, current => current.LastName, Comparer<string>.Default);
+            var actual = JoeyOrderByLastNameAndFirstName(employees, current => current.LastName, Comparer<string>.Default, current1 => current1.FirstName, Comparer<string>.Default);
 
             var expected = new[]
             {
@@ -60,7 +60,9 @@ namespace CSharpAdvanceDesignTests
         private IEnumerable<Employee> JoeyOrderByLastNameAndFirstName(
             IEnumerable<Employee> employees,
             Func<Employee, string> firstKeySelector,
-            Comparer<string> firstKeyComparer)
+            Comparer<string> firstKeyComparer,
+            Func<Employee, string> secondKeySelector,
+            Comparer<string> secondKeyComparer)
         {
             var elements = employees.ToList();
             while (elements.Any())
@@ -70,14 +72,14 @@ namespace CSharpAdvanceDesignTests
                 for (int i = 1; i < elements.Count; i++)
                 {
                     var current = elements[i];
-                    if (firstKeyComparer.Compare(firstKeySelector(current), minElement.LastName) < 0)
+                    if (firstKeyComparer.Compare(firstKeySelector(current), firstKeySelector(minElement)) < 0)
                     {
                         minElement = current;
                         index = i;
                     }
-                    else if (Comparer<string>.Default.Compare(firstKeySelector(current), minElement.LastName) == 0)
+                    else if (firstKeyComparer.Compare(firstKeySelector(current), firstKeySelector(minElement)) == 0)
                     {
-                        if (Comparer<string>.Default.Compare(current.FirstName, minElement.FirstName) < 0)
+                        if (secondKeyComparer.Compare(secondKeySelector(current), secondKeySelector(minElement)) < 0)
                         {
                             minElement = current;
                             index = i;
