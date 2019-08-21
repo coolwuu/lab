bcfbe35abb80874a5921ed8fd3786157a081a224
Wuu <coolwuu0207@gmail.com>
2019-08-21 08:36:47 +0800

    [Skip] add failed test

--- CSharpAdvanceDesignTests/JoeySkipTests.cs
+++ CSharpAdvanceDesignTests/JoeySkipTests.cs
@@ -7,13 +7,19 @@ using System.Linq;
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture]
-    [Ignore("not yet")]
     public class JoeySkipTests
     {
         [Test]
         public void skip_2_employees()
         {
-            var employees = GetEmployees();
+            var employees = new List<Employee>
+            {
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "Tom", LastName = "Li"},
+                new Employee {FirstName = "David", LastName = "Chen"},
+                new Employee {FirstName = "Mike", LastName = "Chang"},
+                new Employee {FirstName = "Joseph", LastName = "Yao"},
+            };
 
             var actual = JoeySelect(employees);
 
@@ -30,18 +36,6 @@ namespace CSharpAdvanceDesignTests
         private IEnumerable<Employee> JoeySelect(IEnumerable<Employee> employees)
         {
             throw new System.NotImplementedException();
-        }
-
-        private static IEnumerable<Employee> GetEmployees()
-        {
-            return new List<Employee>
-            {
-                new Employee {FirstName = "Joey", LastName = "Chen"},
-                new Employee {FirstName = "Tom", LastName = "Li"},
-                new Employee {FirstName = "David", LastName = "Chen"},
-                new Employee {FirstName = "Mike", LastName = "Chang"},
-                new Employee {FirstName = "Joseph", LastName = "Yao"},
-            };
         }
     }
 }
