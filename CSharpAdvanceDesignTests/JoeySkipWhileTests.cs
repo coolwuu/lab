0263738f620c677430ca7c9c3b07ce1c8ab78c31
Wuu <coolwuu0207@gmail.com>
2019-08-21 09:27:23 +0800

    [SkipWhile] Add Fail test

--- CSharpAdvanceDesignTests/JoeySkipWhileTests.cs
+++ CSharpAdvanceDesignTests/JoeySkipWhileTests.cs
@@ -2,6 +2,7 @@
 using Lab.Entities;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -35,6 +36,37 @@ namespace CSharpAdvanceDesignTests
             };
 
             expected.ToExpectedObject().ShouldEqual(actual.ToList());
+        }
+
+        [Test]
+        public void skip_cards_when_point_less_than_5()
+        {
+            var cards = new List<Card>
+            {
+                new Card {Kind = CardKind.Normal, Point = 2},
+                new Card {Kind = CardKind.Normal, Point = 3},
+                new Card {Kind = CardKind.Normal, Point = 4},
+                new Card {Kind = CardKind.Separate},
+                new Card {Kind = CardKind.Normal, Point = 5},
+                new Card {Kind = CardKind.Normal, Point = 6},
+                new Card {Kind = CardKind.Separate},
+            };
+
+            var actual = JoeySkipWhilePointLessThan5(cards);
+
+            var expected = new List<Card>
+            {
+                new Card {Kind = CardKind.Normal, Point = 5},
+                new Card {Kind = CardKind.Normal, Point = 6},
+                new Card {Kind = CardKind.Separate},
+            };
+
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        private IEnumerable<Card> JoeySkipWhilePointLessThan5(IEnumerable<Card> cards)
+        {
+            throw new NotImplementedException();
         }
 
         private IEnumerable<Card> JoeySkipWhile(IEnumerable<Card> cards)
