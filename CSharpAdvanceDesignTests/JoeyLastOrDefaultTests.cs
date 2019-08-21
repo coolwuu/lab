d8c13e15005c64def41369f631884cf30ebd50be
Wuu <coolwuu0207@gmail.com>
2019-08-21 15:41:48 +0800

    [LastOrDefault] Add fail test and pass

--- CSharpAdvanceDesignTests/JoeyLastOrDefaultTests.cs
+++ CSharpAdvanceDesignTests/JoeyLastOrDefaultTests.cs
@@ -1,4 +1,5 @@
-﻿using Lab.Entities;
+﻿using ExpectedObjects;
+using Lab.Entities;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
 using System.Collections.Generic;
@@ -14,6 +15,36 @@ namespace CSharpAdvanceDesignTests
             var employees = new List<Employee>();
             var actual = JoeyLastOrDefault(employees);
             Assert.IsNull(actual);
+        }
+
+        [Test]
+        public void get_last_employee_who_is_manager()
+        {
+            var employees = new List<Employee>()
+            {
+                new Employee() {FirstName = "Joey", Role = Role.Manager},
+                new Employee() {FirstName = "David", Role = Role.Designer},
+                new Employee() {FirstName = "Tom", Role = Role.Manager},
+                new Employee() {FirstName = "May", Role = Role.Engineer},
+            };
+            var actual = JoeyLastOrDefaultWithCondition(employees);
+            new Employee() { FirstName = "Tom", Role = Role.Manager }.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        private Employee JoeyLastOrDefaultWithCondition(IEnumerable<Employee> employees)
+        {
+            var enumerator = employees.GetEnumerator();
+            var result = default(Employee);
+            while (enumerator.MoveNext())
+            {
+                var current = enumerator.Current;
+                if (current.Role == Role.Manager)
+                {
+                    result = current;
+                }
+            }
+
+            return result;
         }
 
         private Employee JoeyLastOrDefault(IEnumerable<Employee> employees)
