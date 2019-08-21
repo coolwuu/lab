635105b43573ebb6acac9ca482f0de69ecb91cfe
Wuu <coolwuu0207@gmail.com>
2019-08-21 10:30:44 +0800

    [Any] Pass test with condition

--- CSharpAdvanceDesignTests/JoeyAnyTests.cs
+++ CSharpAdvanceDesignTests/JoeyAnyTests.cs
@@ -1,6 +1,7 @@
 ﻿using Lab;
 using Lab.Entities;
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace CSharpAdvanceDesignTests
 {
@@ -30,6 +31,29 @@ namespace CSharpAdvanceDesignTests
 
             var actual = emptyEmployees.JoeyAny();
             Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void any_number_greater_than_91()
+        {
+            var numbers = new[] { 87, 88, 91, 93, 0 };
+            var actual = JoeyAnyWithCondition(numbers);
+            Assert.IsTrue(actual);
+        }
+
+        private bool JoeyAnyWithCondition(IEnumerable<int> source)
+        {
+            var enumerator = source.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                var current = enumerator.Current;
+                if (current > 91)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
